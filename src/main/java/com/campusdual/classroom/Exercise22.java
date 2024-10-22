247a577a16a3a5572f6e1bf75179c5dbdd864e00
EmmaIP <emmapineiro@gmail.com>
2024-10-22 12:33:03 +0200

    ejercicio 22 finalizado

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -8,17 +8,40 @@ public class Exercise22 {
 
     public static List<String> createArrayList() {
 
+        List<String> elementList = new ArrayList<>();
+
+        elementList.add("GKFFD");
+        elementList.add("TNANA");
+        elementList.add("MPMSL");
+        elementList.add("PSWME");
+        elementList.add("LZMLF");
+        elementList.add("JYEBV");
+        elementList.add("YELNT");
+        elementList.add("JSNKR");
+        elementList.add("JFESF");
+        elementList.add("TMJLL");
+
+        return elementList;
+
     }
 
     public static void printElementsAndIndex(List<String> customList) {
-
+        for (String c: customList) {
+            System.out.println("El elemento vale " + c + " y su índice en la lista es " + customList.indexOf(c));
+        }
     }
 
     public static boolean addElementToList(List<String> customList, String element) {
+        customList.add(element);
+        return true;
 
     }
 
     public static void main(String[] args) {
+        createArrayList();
+        String element = "AAAAA";
+        addElementToList(createArrayList(), element);
+        printElementsAndIndex(createArrayList());
 
     }
 
